c46db37feb1d75fb9d4a4cacf8f396416ff0d083
Антон Реут <puremag1c@MacBook-Air-Anton.local>
2020-04-15 01:44:13 +0300

    mee

--- lib/cards.ex
+++ lib/cards.ex
@@ -7,9 +7,15 @@ defmodule Cards do
   @doc """
     Хуярит колоду
   """
+
+  def fii do
+    dddd = 2 * 2
+    dddd
+  end
+
   def create do
-    values = ["Ace","two","three", "King"]
-    suits = ["clubs","Diamonds"]
+    values = ["Ace", "two", "three", "King"]
+    suits = ["clubs", "Diamonds"]
 
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
@@ -22,32 +28,35 @@ defmodule Cards do
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+
   @doc """
     ебля
   """
   def contains?(deck, el) do
-    Enum.member?(deck,el)
+    Enum.member?(deck, el)
   end
+
   @doc """
     Спрашивает сколько карт вытащить из колоды
   """
   def count do
-    IO.gets("? ") |> String.trim |> String.to_integer
+    IO.gets("? ") |> String.trim() |> String.to_integer()
   end
+
   @doc """
     выдает из колоды скока надо
   """
   def deal(deck) do
-    size = Cards.count
-    Enum.split(deck,size)
+    size = Cards.count()
+    Enum.split(deck, size)
   end
+
   @doc """
     выполняет всю хуйню разом
   """
   def go do
-    {hand, _other} = Cards.create |> Cards.shuffle |> Cards.deal
+    {hand, _other} = Cards.create() |> Cards.shuffle() |> Cards.deal()
     hand
     other
   end
-
 end
